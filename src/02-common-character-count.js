5133d5aa279c52861050fd551c7f829fd20cc86d
egor antrop <mougraaa@gmail.com>
2021-04-10 22:44:29 +0400

    feat: implemented 3 task

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,20 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  let coincidence = 0;
+  const arr1 = s1.split('').sort();
+  const arr2 = s2.split('').sort();
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.indexOf(arr1[i]) !== -1) {
+      coincidence++;
+      arr2.splice(arr2.indexOf(arr1[i]), 1);
+      arr1.splice(i, 1);
+      i--;
+    }
+  }
+  return coincidence;
 }
 
 module.exports = getCommonCharacterCount;
--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,32 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  // const without = -1;
+  // const sort = arr.sort();
+  function sortNumbers(a, b) {
+    if (a > b) {
+      return 1;
+    }
+    if (b > a) {
+      return -1;
+    }
+    return 0;
+  }
+  const possition = [];
+  let possitionHelper = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.indexOf(-1) === -1) break;
+    possition.push(arr.indexOf(-1) + possitionHelper);
+    arr.splice(arr.indexOf(-1), 1);
+    possitionHelper++;
+    i--;
+  }
+  arr.sort(sortNumbers);
+  for (let i = 0; i < possition.length; i++) {
+    arr.splice(possition[i], 0, -1);
+  }
+  return arr;
 }
 
 module.exports = sortByHeight;
